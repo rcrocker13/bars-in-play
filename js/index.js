f57ab2cd14fee67668eef1f8caef68b29a97e8f8
Seemant Kulleen <seemantk@gmail.com>
2017-01-07 10:26:21 -0800

    OK, many changes here.  First, the bar enter/update/exit pattern is moved into the update_data() function, which gets called whenever the opponent team select box value changes.
    Bars now transition to update for the new team.
    
    TODO: the 1/2 bar is a bit wonky still.  And colors are a bit...hmm.

--- js/index.js
+++ js/index.js
@@ -14,6 +14,10 @@ var padding = {top: 40, right: 60, bottom: 40, left: 80}
         .domain(["LD", "LC", "LB", "LA/RA", "RB", "RC", "RD"])
         .range(["7", "5", "3", "1/2", "4", "6", "8"])
         .unknown(null)
+  , colorExtents = {
+          average: []
+        , expected: []
+      }
 ;
 
 var canvas = d3.select("#chart").append("svg")
@@ -23,31 +27,21 @@ var canvas = d3.select("#chart").append("svg")
     .attr("transform", "translate(" + padding.left + ", " + padding.top + ")");
 
 d3.queue()
-    .defer(d3.csv, "data/run-gap.csv", preprocess)
-    .defer(d3.csv, "data/og-data.csv", processrow)
+    .defer(d3.csv, "data/og-data.csv", preprocess)
     .await(main_function)
 ;
 
 function preprocess(row) {
-    row.count = +row.count;
-    row.avgSuccess = +row.avgSuccess;
-    row.expSuccess = +row.expSuccess;
+  row.success = row["Success?"] === "Yes" ? 1 : 0;
 
-    // MUST do this!
-    return row;
+  row.success_resid  = row.success - 0.432;
+  row.expected_resid = row.success - row.ExpectedEP;
+
+  // MUST do this!
+  return row;
 } // preprocess()
 
-function processrow(row) {
-    row.success = row["Success?"] === "Yes" ? 1 : 0;
-
-    row.success_resid = row.success - 0.432;
-    row.expected_resid = row.success - row.ExpectedEP;
-
-    // MUST do this!
-    return row;
-} // processrow()
-
-function main_function(error, data, og) {
+function main_function(error, data) {
     if (error) throw error;
 
     // arrange the data in a hierarchy by:
@@ -57,9 +51,9 @@ function main_function(error, data, og) {
         // Run direction
         .key(function(d) { return d.RunDir; })
         // First, filter dataset of nulls, "Trick Play"s, etc
-        .map(og.filter(function(d) { return rungaps(d.RunDir); }))
+        .map(data.filter(function(d) { return rungaps(d.RunDir); }))
     ;
-    console.log(data, og, opponents);
+    console.log(data, opponents);
 
     // Populate the select box with the team name
     var opt = d3.select("#opponent")
@@ -74,52 +68,9 @@ function main_function(error, data, og) {
     ;
     d3.select("#opponent")
         .on("change", function() {
-            console.log(opponents.get(this.value).entries());
+            update_data(opponents.get(this.value));
           })
     ;
-    colorScale
-        .range([0, 1])
-        .domain(d3.extent(data, function(d) { return d.avgSuccess; }))
-    ;
-    x
-        .domain(data.map(function(d) { return d.runGap; }))
-    ;
-
-    var max = d3.max(data, function(d) { return d.count; });
-    y
-        .domain([0, max])
-        .range([height, 0])
-    ;
-    canvas.append("g")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(xAxis)
-    ;
-    canvas.append("g")
-        .call(yAxis)
-    ;
-    var bar = canvas.selectAll(".bar")
-        .data(data)
-    ;
-
-    bar.enter()
-      .append("rect")
-        // class our rects to .bar so the selection can find their target
-        .attr("class", "bar")
-        .style("fill", function(d) {
-            return d3.interpolateWarm(colorScale(d[selectedOption]));
-          })
-        .attr("x", function(d, i) {
-            return i * x.step();
-          })
-        .attr("width", x.bandwidth())
-        .attr("y", function(d) {
-            return y(d.count);
-          })
-        .attr("height", function(d) {
-            return height - y(d.count);
-          })
-    ;
-
     var selectedOption
       , colorExtent = []
     ;
@@ -140,4 +91,78 @@ function main_function(error, data, og) {
 
           })
     ;
-} // main_function()
+    /*
+    ** Set up the initial axes
+    */
+    canvas.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0, " + height + ")")
+        .call(xAxis)
+    ;
+    canvas.append("g")
+        .attr("class", "axis axis--y")
+        .call(yAxis)
+    ;
+
+    /*
+    ** Initialize the visualization
+    */
+    update_data(opponents.get(d3.select("#opponent").node().value));
+  } // main_function()
+
+function update_data(data) {
+    console.log(data);
+    colorScale
+        .range([0, 1])
+        .domain(d3.extent(d3.merge(data.values()), function(d) { return d.success_resid; }))
+    ;
+    x
+        .domain(rungaps.range())
+    ;
+
+    var max = d3.max(data.values().map(function(d) { return d.length; }));
+    y
+        .domain([0, max])
+    ;
+    var bar = canvas.selectAll(".bar")
+        .data(data.entries(), function(d) { return d.key; })
+    ;
+    bar = bar.enter()
+      .append("rect")
+        // class our rects to .bar so the selection can find their target
+        .attr("class", "bar")
+        // .style("fill", function(d) {
+        //     return d3.interpolateWarm(colorScale(d[selectedOption]));
+        //   })
+        .attr("x", function(d, i) {
+            return x(rungaps(d.key));
+          })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) {
+            return y(0);
+          })
+        .attr("height", 0)
+      .merge(bar)
+    ;
+    bar
+      .transition()
+        .attr("y", function(d) {
+            return y(d.value.length);
+          })
+        .attr("height", function(d) {
+            return height - y(d.value.length);
+          })
+    ;
+    d3.select(".axis--x")
+      .transition()
+        .call(xAxis.scale(x))
+    ;
+    d3.select(".axis--y")
+      .transition()
+        .call(yAxis.scale(y))
+    ;
+    bar.exit()
+      .transition()
+        .attr("height", 0)
+    ;
+} // update_data()
