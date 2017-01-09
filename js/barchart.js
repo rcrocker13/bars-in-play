8fba5aa94bfdfb16deb2d8fa0a0607841c3ef6d4
Seemant Kulleen <seemantk@gmail.com>
2017-01-09 09:23:59 -0800

    Migrate barchart specific things into the BarChart class. Now, let's try to generate the second chart with this function object.

--- js/barchart.js
+++ js/barchart.js
@@ -2,32 +2,98 @@ function BarChart() {
     /*
     ** Private Variables
     */
-    var svg
+    var canvas
       , margin
       , data
       , title
+      , padding = {top: 40, right: 60, bottom: 40, left: 80}
+      , height = 200
+      , width = 200
+      , x = d3.scaleBand()
+            .range([0, width])
+            .paddingInner(0.1)
+      , y = d3.scaleLinear()
+            .range([height, 0 ])
+      , xAxis = d3.axisBottom(x)
+      , yAxis = d3.axisLeft(y)
     ;
     /*
     ** Main Function Object
     */
     function my(sel) {
         title = title || "barchart" + Math.round(Math.random() * 10000);
-        svg = sel.selectAll("svg")
-            .data([title], function(d) { return d; })
+        var svg = sel.selectAll("svg")
+            .data([1], function(d) { return d; })
         ;
-        svg.enter()
+        svg = svg.enter()
           .append("svg")
             .style("width",  "100%")
             .style("height", "100%")
-          .append("title")
-            .text(title)
+            .attr("height", height + padding.top + padding.bottom)
+            .attr("width", width + padding.left + padding.right)
+          .each(function(d) {
+              d3.select(this)
+                .append("title")
+                  .text(title)
+              ;
+            })
+          .merge(svg)
         ;
-        console.log(svg)
+        canvas = svg
+          .append("g")
+            .attr("transform", "translate(" + padding.left + ", " + padding.top + ")")
+        ;
+        canvas.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0, " + height + ")")
+            .call(xAxis.scale(x)) // this axis won't change
+        ;
+        canvas.append("g")
+            .attr("class", "axis axis--y")
+            .call(yAxis)
+        ;
     } // main function object
 
     /*
     ** Private Helper Functions
     */
+    function update() {
+        var bar = canvas.selectAll(".bar")
+            .data(data.entries(), function(d) { return d.key; })
+        ;
+        bar.enter()
+          .append("rect")
+            // class our rects to .bar so the selection can find their target
+            .attr("class", "bar")
+            .attr("x", function(d, i) {
+                return x(rungaps(d.key));
+              })
+            .attr("width", x.bandwidth())
+            .attr("y", function(d) { return y(0); })
+            .attr("height", 0)
+          .merge(bar)
+          .transition()
+            .attr("y", function(d) { return y(d.value.count); })
+            .attr("height", function(d) { return height - y(d.value.count); })
+            .style("fill", function(d) {
+                return d3.interpolateWarm(colorScale(d.value.avgSuccess));
+              })
+        ;
+        d3.select(".axis--y")
+          .transition()
+            .call(yAxis.scale(y))
+        ;
+        d3.select(".axis--x")
+          .transition()
+            .call(xAxis.scale(x))
+        ;
+        bar.exit()
+          .transition()
+            .attr("y", y(0))
+            .attr("height", 0)
+        ;
+        //update_colors(d3.select("#color").node().value);
+    }
 
     /*
     ** API: Getter/Setter Functions
@@ -38,7 +104,27 @@ function BarChart() {
         return my;
       } // my.title()
     ;
-
+    my.xdomain = function(_) {
+        if(!arguments.length) return x.domain();
+        x.domain(_);
+        return my;
+      } // my.xdomain()
+    ;
+    my.data = function(_) {
+        if(!arguments.length) return data;
+        data = _;
+        var max = d3.max(data.values(), function(d) { return d.count; });
+        y
+            .domain([0, max])
+        ;
+        return my;
+      } // my.data()
+    ;
+    my.update = function(_) {
+        update();
+        return my;
+     } // my.update()
+    ;
     // This is ALWAYS the last thing returned
     return my;
 } // function object BarChart()
--- js/index.js
+++ js/index.js
@@ -1,30 +1,13 @@
-var padding = {top: 40, right: 60, bottom: 40, left: 80}
-  , height = 150
-  , width = 200
-  , rungaps = d3.scaleOrdinal()
+var rungaps = d3.scaleOrdinal()
         .domain(["LD", "LC", "LB", "LA/RA", "RB", "RC", "RD"])
         .range(["7", "5", "3", "1/2", "4", "6", "8"])
         .unknown(null)
-  , x = d3.scaleBand()
-        .domain(rungaps.range())
-        .range([0, width])
-        .paddingInner(0.1)
-  , y = d3.scaleLinear()
-        .range([height, 0 ])
-  , xAxis = d3.axisBottom(x)
-  , yAxis = d3.axisLeft(y)
-        .ticks(4)
   , colorExtents = {
           avgSuccess: []
         , expSuccess: []
       }
   , colorScale = d3.scaleLinear()
-;
-var canvas = d3.select("#chart").append("svg")
-    .attr("height", height + padding.top + padding.bottom)
-    .attr("width", width + padding.left + padding.right)
-  .append("g")
-    .attr("transform", "translate(" + padding.left + ", " + padding.top + ")")
+  , chartRungap = BarChart().xdomain(rungaps.range())
 ;
 
 
@@ -46,27 +29,47 @@ function preprocess(row) {
 function main_function(error, data) {
     if (error) throw error;
 
-    colorExtents.avgSuccess = d3.extent(data, function(d) { return d.success_resid; });
-    colorExtents.expSuccess = d3.extent(data, function(d) { return d.expected_resid; });
+    function success_resid(l) { return l.success_resid; }
+    function expected_resid(l) { return l.expected_resid; }
+
+    colorExtents.avgSuccess = d3.extent(data, success_resid);
+    colorExtents.expSuccess = d3.extent(data, expected_resid);
 
     // arrange the data in a hierarchy by:
     var opponents = d3.nest()
         // Opposing team name
         .key(function(d) { return d.Defense; })
         // Run direction
-        .key(function(d) { return d.RunDir; })
         // Populate with only the data we need
         .rollup(function(leaves) {
-            return {
-                count: leaves.length
-              , avgSuccess: d3.mean(leaves, function(l) { return l.success_resid; })
-              , expSuccess: d3.mean(leaves, function(l) { return l.expected_resid; })
-            };
+              return {
+                  rundirs: d3.nest()
+                      .key(function(d) { return d.RunDir; })
+                      .rollup(function(leaves) {
+                          return {
+                              count: leaves.length
+                            , avgSuccess: d3.mean(leaves, success_resid)
+                            , expSuccess: d3.mean(leaves, expected_resid)
+                            , plays: leaves
+                          };
+                        })
+                      .map(leaves)
+                , reads: d3.nest()
+                      .key(function(d) { return d.PostRead1; })
+                      .rollup(function(leaves) {
+                          return {
+                              count: leaves.length
+                            , avgSuccess: d3.mean(leaves, success_resid)
+                            , expSuccess: d3.mean(leaves, expected_resid)
+                            , plays: leaves
+                          };
+                        })
+                      .map(leaves)
+              };
           })
         // But first, filter dataset of nulls, "Trick Play"s, etc
         .map(data.filter(function(d) { return rungaps(d.RunDir); }))
     ;
-    console.log(opponents);
     // Populate the select box with the team name
     var opt = d3.select("#opponent")
         .append("optgroup") //enhance UX
@@ -79,87 +82,31 @@ function main_function(error, data) {
         .text(function(d) { return d; })
     ;
     d3.select("#opponent")
-        .on("change", function() {
-            update(opponents.get(this.value));
-          })
+        .on("change", function() { update(opponents.get(this.value).rundirs); })
     ;
     d3.select("#color")
-        .on("change", function() {
-            update_colors(this.value);
-          })
+        .on("change", function() { update_colors(this.value); })
     ;
     /*
-    ** Set up the initial axes
+    ** Set up the initial svg
     */
-    canvas.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(xAxis.scale(x)) // this axis won't change
+    d3.select("#chart-rungap")
+        .call(chartRungap)
     ;
-    canvas.append("g")
-        .attr("class", "axis axis--y")
-        .call(yAxis)
-    ;
-
     /*
     ** Initialize the visualization
     */
-    update(opponents.get(d3.select("#opponent").node().value));
-  } // main_function()
+    update(opponents.get(d3.select("#opponent").node().value).rundirs);
+    console.log(data, opponents);
+} // main_function()
 
 function update(data) {
-    console.log(data);
     var selectedColor = d3.select("#color").node().value;
     colorScale.domain(colorExtents[selectedColor]);
-
-    var max = d3.max(data.values(), function(d) { return d.count; });
-    y
-        .domain([0, max])
+    chartRungap
+        .data(data)
+        .update()
     ;
-    var bar = canvas.selectAll(".bar")
-        .data(data.entries(), function(d) { return d.key; })
-    ;
-    bar.enter()
-      .append("rect")
-        // class our rects to .bar so the selection can find their target
-        .attr("class", "bar")
-        // .style("fill", function(d) {
-        //     return d3.interpolateWarm(colorScale(d[selectedOption]));
-        //   })
-        .attr("x", function(d, i) {
-            return x(rungaps(d.key));
-          })
-        .attr("width", x.bandwidth())
-        .attr("y", function(d) {
-            return y(0);
-          })
-        .attr("height", 0)
-      .merge(bar)
-      .transition()
-        .attr("y", function(d) {
-            return y(d.value.count);
-          })
-        .attr("height", function(d) {
-            return height - y(d.value.count);
-          })
-        .style("fill", function(d) {
-            return d3.interpolateWarm(colorScale(d.value.avgSuccess));
-          })
-    ;
-    d3.select(".axis--y")
-      .transition()
-        .call(yAxis.scale(y))
-    ;
-    d3.select(".axis--x")
-      .transition()
-        .call(xAxis.scale(x))
-    ;
-    bar.exit()
-      .transition()
-        .attr("y", y(0))
-        .attr("height", 0)
-    ;
-    //update_colors(d3.select("#color").node().value);
 } // update()
 
 function update_colors(selectedOption) {
